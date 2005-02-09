bfce1e5595e187ba2522e9d00ebce2d5cbba2208
Martin 'Kingpin' Thurn <kingpin@dcswcc.org>
2005-02-09 02:43:11 +0000

    Import of MTHURN/Devel-Fail-Make-1.006 from CPAN.
    gitpan-cpan-distribution: Devel-Fail-Make
    gitpan-cpan-version:      1.006
    gitpan-cpan-path:         MTHURN/Devel-Fail-Make-1.006.tar.gz
    gitpan-cpan-author:       MTHURN
    gitpan-cpan-maturity:     released

---
+++ lib/Devel/Fail/Make.pm
@@ -0,0 +1,7 @@
+# This dummy/empty module exists only so that it gets indexed in the
+# CPAN module list.
+package Devel::Fail::Make;
+my
+$VERSION = do { my @r = (q$Revision: 1.1 $ =~ /\d+/g); sprintf "%d."."%03d" x $#r, @r };
+1;
+__END__
